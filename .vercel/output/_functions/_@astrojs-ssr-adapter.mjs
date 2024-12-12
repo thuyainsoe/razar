08e7c144e7288f6622d567d9a21f1254a98b52a8
thuyainsoe <thuyainsoe163361@gmail.com>
2024-12-12 21:22:39 +0700

    first commit

---
+++ .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
@@ -0,0 +1,1 @@
+export { c as createExports, s as start } from './chunks/entrypoint_CKn6pP1w.mjs';
---
+++ .vercel/output/_functions/entry.mjs
@@ -0,0 +1,30 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports } from './chunks/entrypoint_CKn6pP1w.mjs';
+import { manifest } from './manifest_rKTJMzUF.mjs';
+
+const serverIslandMap = new Map([
+]);;
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/players.astro.mjs');
+const _page2 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/players/index.astro", _page1],
+    ["src/pages/index.astro", _page2]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "9815d5a4-a31b-4db1-ab15-a3c017032ea8",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
