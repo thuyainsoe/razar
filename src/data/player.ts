5c0c5da44258744f106d625f6501efb5942d7406
thuyainsoe <thuyainsoe163361@gmail.com>
2024-12-12 17:26:22 +0700

    first commit

---
+++ src/data/player.ts
@@ -0,0 +1,666 @@
+export type PlayerData = {
+  name: string;
+  ingame_name: string;
+  team: {
+    name: string;
+    logo: string;
+  } | null;
+  country: {
+    name: string;
+    logo: string;
+  };
+};
+
+export const playerData: PlayerData[] = [
+  {
+    name: "Htoo Aung Thit",
+    ingame_name: "Alitaaa",
+    team: {
+      name: "AI Esports",
+      logo: "/images/myanmar/team_icon/ai.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Phone Myint Kyi",
+    ingame_name: "Ardam",
+    team: null,
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Min Swan Kon",
+    ingame_name: "Beni",
+    team: {
+      name: "Falcon Esports",
+      logo: "/images/myanmar/team_icon/falcon.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Kyaw Khaung Htut",
+    ingame_name: "Benno",
+    team: {
+      name: "Falcon Esports",
+      logo: "/images/myanmar/team_icon/falcon.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Tain Yan Lin",
+    ingame_name: "Bird",
+    team: {
+      name: "Zino Esports",
+      logo: "/images/myanmar/team_icon/zino.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Thet Naing Zaw",
+    ingame_name: "Black",
+    team: {
+      name: "AI Esports",
+      logo: "/images/myanmar/team_icon/ai.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Htoo Htet Kyaw",
+    ingame_name: "Blank",
+    team: null,
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Kyaw Thuya",
+    ingame_name: "Blink",
+    team: {
+      name: "Burmese Ghouls",
+      logo: "/images/myanmar/team_icon/bg.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Yan Moe Aung",
+    ingame_name: "Bloodkikaka",
+    team: {
+      name: "Burmese Ghouls",
+      logo: "/images/myanmar/team_icon/bg.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Thiha Kyaw",
+    ingame_name: "Carbon",
+    team: {
+      name: "Blade Violet",
+      logo: "/images/myanmar/team_icon/bv.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Hsu Yamin Htet",
+    ingame_name: "Cassie",
+    team: null,
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Wint Yadanar Linn",
+    ingame_name: "Cko",
+    team: {
+      name: "Blade Angels",
+      logo: "/images/myanmar/team_icon/ba.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Thuya Aung",
+    ingame_name: "DK",
+    team: null,
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Kaung Sett",
+    ingame_name: "Daxx",
+    team: {
+      name: "Falcon Esports",
+      logo: "/images/myanmar/team_icon/falcon.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Kyaw Hein Zin",
+    ingame_name: "FayYan",
+    team: {
+      name: "AI Esports",
+      logo: "/images/myanmar/team_icon/ai.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Zaw Thu Hein",
+    ingame_name: "Gumi",
+    team: null,
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Yadanar Hlaing",
+    ingame_name: "Hlaing",
+    team: {
+      name: "Blade Angels",
+      logo: "/images/myanmar/team_icon/ba.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Aung Htut Myat",
+    ingame_name: "Htut Htut",
+    team: {
+      name: "Zino Esports",
+      logo: "/images/myanmar/team_icon/zino.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Kaung Min Khant",
+    ingame_name: "J3X",
+    team: {
+      name: "Falcon Esports",
+      logo: "/images/myanmar/team_icon/falcon.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Nyi Ju No",
+    ingame_name: "Juno",
+    team: {
+      name: "Young Legion",
+      logo: "/images/myanmar/team_icon/yl.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Pyae Sone Khant",
+    ingame_name: "JustiN",
+    team: {
+      name: "Mythic SEAL",
+      logo: "/images/myanmar/team_icon/ms.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Hsu Labb Nyi Nyi Maung",
+    ingame_name: "KELL",
+    team: {
+      name: "Net Angels",
+      logo: "/images/myanmar/team_icon/na.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Pyae Hein Ko",
+    ingame_name: "Kenn",
+    team: {
+      name: "Mythic SEAL",
+      logo: "/images/myanmar/team_icon/ms.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Khant Khant",
+    ingame_name: "Khant Khant",
+    team: {
+      name: "Team Star Boyz",
+      logo: "/images/myanmar/team_icon/ts.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Khant Thurein Aung",
+    ingame_name: "Khant Thurein",
+    team: {
+      name: "Zino Esports",
+      logo: "/images/myanmar/team_icon/zino.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Hein Min Thu",
+    ingame_name: "Kid",
+    team: null,
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Yehtet Sithu",
+    ingame_name: "KidX",
+    team: {
+      name: "Falcon Esports",
+      logo: "/images/myanmar/team_icon/falcon.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Hsu Wai Zin",
+    ingame_name: "Kuu",
+    team: {
+      name: "Blade Angels",
+      logo: "/images/myanmar/team_icon/ba.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Thet Htar Thinzar",
+    ingame_name: "LICCA",
+    team: {
+      name: "Net Angels",
+      logo: "/images/myanmar/team_icon/na.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Thura Pyae Sone",
+    ingame_name: "Light",
+    team: {
+      name: "Zino Esports",
+      logo: "/images/myanmar/team_icon/zino.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Kaung Min Khant",
+    ingame_name: "Lina",
+    team: {
+      name: "AI Esports",
+      logo: "/images/myanmar/team_icon/ai.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Win Min Htoo",
+    ingame_name: "Meow",
+    team: null,
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Moe Ko Ko",
+    ingame_name: "Mok",
+    team: {
+      name: "Falcon Esports",
+      logo: "/images/myanmar/team_icon/falcon.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Min Ko Ko",
+    ingame_name: "Naomi",
+    team: null,
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Hein Arkar Htet",
+    ingame_name: "Niko",
+    team: {
+      name: "Burmese Ghouls",
+      logo: "/images/myanmar/team_icon/bg.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Khant Kyaw Zay",
+    ingame_name: "OHMYJ3NNIE",
+    team: {
+      name: "AI Esports",
+      logo: "/images/myanmar/team_icon/ai.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Si Thu Min Htet",
+    ingame_name: "Policy",
+    team: {
+      name: "Burmese Ghouls",
+      logo: "/images/myanmar/team_icon/bg.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Wai Phyo Aung",
+    ingame_name: "Ps",
+    team: {
+      name: "AI Esports",
+      logo: "/images/myanmar/team_icon/ai.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Yar Zar Mon",
+    ingame_name: "Px7",
+    team: {
+      name: "Falcon Esports",
+      logo: "/images/myanmar/team_icon/falcon.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Arkar Bhone Pyae",
+    ingame_name: "Royal Milk",
+    team: {
+      name: "Falcon Esports",
+      logo: "/images/myanmar/team_icon/falcon.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Swan Htet Aung",
+    ingame_name: "Ruby DD",
+    team: {
+      name: "Mythic SEAL",
+      logo: "/images/myanmar/team_icon/ms.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Htet Wai Kywal",
+    ingame_name: "SAYU",
+    team: {
+      name: "Net Angels",
+      logo: "/images/myanmar/team_icon/na.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Kaung Htet Hein",
+    ingame_name: "Shadow",
+    team: null,
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Hlwan Moe Htet",
+    ingame_name: "Shine CG",
+    team: {
+      name: "9K EsportsxMG",
+      logo: "/images/myanmar/team_icon/9k.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Phyo Kyaw Hlaing",
+    ingame_name: "Shinshinkick",
+    team: null,
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Pyae Sone",
+    ingame_name: "Silent",
+    team: null,
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Kaung Khant Min",
+    ingame_name: "Simple",
+    team: null,
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Htet Linn Hlyan Aung",
+    ingame_name: "Stitch",
+    team: {
+      name: "Burmese Ghouls",
+      logo: "/images/myanmar/team_icon/bg.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Ye Htut Lwin",
+    ingame_name: "Tychon",
+    team: {
+      name: "Young Legion",
+      logo: "/images/myanmar/team_icon/yl.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Van Maung",
+    ingame_name: "VI VI",
+    team: null,
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Khant Paing Soe Htet",
+    ingame_name: "Vam Vam",
+    team: {
+      name: "Team Star Boyz",
+      logo: "/images/myanmar/team_icon/ts.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Ye Man Wint",
+    ingame_name: "XAM",
+    team: null,
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Thi Ha Aung",
+    ingame_name: "Xiao Yan",
+    team: {
+      name: "Team Star Boyz",
+      logo: "/images/myanmar/team_icon/ts.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Hein Zaw Aung",
+    ingame_name: "Xidde",
+    team: {
+      name: "Mythic SEAL",
+      logo: "/images/myanmar/team_icon/ms.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Kan Kaung",
+    ingame_name: "Yato",
+    team: null,
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Hein Htet Oo",
+    ingame_name: "Ying",
+    team: {
+      name: "AI Esports",
+      logo: "/images/myanmar/team_icon/ai.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Kyaw Zin Bo",
+    ingame_name: "Zippx",
+    team: {
+      name: "Mythic SEAL",
+      logo: "/images/myanmar/team_icon/ms.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+  {
+    name: "Hein Min Khant",
+    ingame_name: "yellowflash",
+    team: {
+      name: "Mythic SEAL",
+      logo: "/images/myanmar/team_icon/ms.png",
+    },
+    country: {
+      name: "Myanmar",
+      logo: "🇲🇲",
+    },
+  },
+];
