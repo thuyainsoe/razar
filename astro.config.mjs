892c5605df8ae6d2e312810ee65b1934863d36fb
thuyainsoe <thuyainsoe163361@gmail.com>
2024-12-14 22:31:02 +0700

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,36 @@
 // @ts-nocheck
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import alpinejs from "@astrojs/alpinejs";
 import vercel from "@astrojs/vercel/serverless";
+import { loadEnv } from "vite";
+
+const { PUBLIC_EMAILJS_USER_ID } = loadEnv(
+  process.env.PUBLIC_EMAILJS_USER_ID,
+  process.cwd(),
+  "",
+);
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), alpinejs()],
   output: "server",
   adapter: vercel(),
+  env: {
+    schema: {
+      PUBLIC_EMAILJS_USER_ID: envField.string({
+        context: "client",
+        access: "public",
+      }),
+      PUBLIC_EMAILJS_SERVICE_ID: envField.string({
+        context: "client",
+        access: "public",
+      }),
+      PUBLIC_EMAILJS_TEMPLATE_ID: envField.string({
+        context: "client",
+        access: "public",
+      }),
+    },
+  },
 });
